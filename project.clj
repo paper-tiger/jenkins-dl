32f1b428336078564fcc89b84fd1a97656754861
- <->
2017-10-10 11:39:38 +0200

    Version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jenkins-dl "0.1.0"
+(defproject jenkins-dl "0.2.0-SNAPSHOT"
   :description "Small Utility for Downloading Artifacts from Jenkins-CI"
   :url "https://github.com/paper-tiger/jenkins-dl"
   :license {:name "Eclipse Public License"
